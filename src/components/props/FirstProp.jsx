14151ba914d9e28c3ffba6d37b14f8746b02f3c7
hit-man-shu <himanshusahoo2019@gmail.com>
2024-02-08 12:25:49 +0530

    add inline css

--- src/components/props/FirstProp.jsx
+++ src/components/props/FirstProp.jsx
@@ -11,12 +11,27 @@ const FirstProp = () => {
   return (
     <div>
       <div className="mx-auto my-12 max-w-xl bg-purple-400 px-20 py-10">
-        <h1 className="text-center text-2xl">Map the Array</h1>
-
+        <h1
+          className="w-full py-3 text-center text-2xl"
+          style={{ background: "teal", color: "white", fontWeight: "bold" }}
+        >
+          Map the Array
+        </h1>
+        <h1
+          className="mt-2 text-center text-2xl"
+          style={{ background: "red", color: "white" }}
+        >
+          Name
+        </h1>
         <ul className="flex flex-col gap-2">
           {arr.map((item) => item.name.map((ele, i) => <li key={i}>{ele}</li>))}
         </ul>
-        <h1 className="text-center text-2xl">Location</h1>
+        <h1
+          className="text-center text-2xl"
+          style={{ background: "red", color: "white" }}
+        >
+          Location
+        </h1>
         <ul>
           {arr.map((item) =>
             item.location.map((ele, i) => <li key={i}>{ele}</li>),
